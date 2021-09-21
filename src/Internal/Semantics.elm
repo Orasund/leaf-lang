50639f79668f710b784acfc80053571f85bba416
Lucas Payr <orasund@gmail.com>
2021-09-21 20:37:30 +0200

    removed debug logs

--- src/Internal/Semantics.elm
+++ src/Internal/Semantics.elm
@@ -206,98 +206,96 @@ evalBuildin buildin context =
 
 evalExp : Exp -> Dict String Field -> Result String Value
 evalExp e context =
-    Debug.log "result" <|
-        case Debug.log "input" e of
-            Variable string ->
-                context
-                    |> Dict.get string
-                    |> Maybe.map (.value >> Ok)
-                    |> Maybe.withDefault (Err ("Can't find variable " ++ string))
+    case e of
+        Variable string ->
+            context
+                |> Dict.get string
+                |> Maybe.map (.value >> Ok)
+                |> Maybe.withDefault (Err ("Can't find variable " ++ string))
 
-            NullExp ->
-                Ok NullVal
+        NullExp ->
+            Ok NullVal
 
-            StringExp string ->
-                Ok (StringVal string)
+        StringExp string ->
+            Ok (StringVal string)
 
-            BoolExp bool ->
-                Ok (BoolVal bool)
+        BoolExp bool ->
+            Ok (BoolVal bool)
 
-            IntExp n ->
-                Ok (IntVal n)
+        IntExp n ->
+            Ok (IntVal n)
 
-            FloatExp n ->
-                Ok (FloatVal n)
+        FloatExp n ->
+            Ok (FloatVal n)
 
-            ListExp list ->
-                list
-                    |> List.foldl
-                        (\a ->
-                            Result.andThen
-                                (\l ->
-                                    context
-                                        |> evalExp a
-                                        |> Result.map (\b -> b :: l)
-                                )
-                        )
-                        (Ok [])
-                    |> Result.map (List.reverse >> ListVal)
+        ListExp list ->
+            list
+                |> List.foldl
+                    (\a ->
+                        Result.andThen
+                            (\l ->
+                                context
+                                    |> evalExp a
+                                    |> Result.map (\b -> b :: l)
+                            )
+                    )
+                    (Ok [])
+                |> Result.map (List.reverse >> ListVal)
 
-            ObjectExp dict ->
-                dict
-                    |> Dict.foldl
-                        (\k a ->
-                            Result.andThen
-                                (\l ->
-                                    context
-                                        |> evalExp a
-                                        |> Result.map (\b -> l |> Dict.insert k b)
-                                )
-                        )
-                        (Ok Dict.empty)
-                    |> Result.map ObjectVal
+        ObjectExp dict ->
+            dict
+                |> Dict.foldl
+                    (\k a ->
+                        Result.andThen
+                            (\l ->
+                                context
+                                    |> evalExp a
+                                    |> Result.map (\b -> l |> Dict.insert k b)
+                            )
+                    )
+                    (Ok Dict.empty)
+                |> Result.map ObjectVal
 
-            FunctionExp maybeString exp ->
-                Ok (FunctionVal maybeString exp)
+        FunctionExp maybeString exp ->
+            Ok (FunctionVal maybeString exp)
 
-            BlockExp block ->
-                context
-                    |> Debug.log "context"
-                    |> Dict.map (\_ v -> { v | access = Read })
-                    |> evalBlock (Debug.log "block" block)
-                    |> Result.map Tuple.first
+        BlockExp block ->
+            context
+                |> Dict.map (\_ v -> { v | access = Read })
+                |> evalBlock block
+                |> Result.map Tuple.first
 
-            Apply exp1 exp2 ->
-                context
-                    |> evalExp exp2
-                    |> Result.andThen
-                        (\v2 ->
-                            case v2 of
-                                FunctionVal string exp3 ->
-                                    context
-                                        |> evalExp
-                                            (string
-                                                |> Maybe.map
-                                                    (\s ->
-                                                        exp3
-                                                            |> substitute s exp1
-                                                    )
-                                                |> Maybe.withDefault exp3
-                                            )
+        Apply exp1 exp2 ->
+            context
+                |> evalExp exp2
+                |> Result.andThen
+                    (\v2 ->
+                        case v2 of
+                            FunctionVal string exp3 ->
+                                context
+                                    |> evalExp
+                                        (string
+                                            |> Maybe.map
+                                                (\s ->
+                                                    exp3
+                                                        |> substitute s exp1
+                                                )
+                                            |> Maybe.withDefault exp3
+                                        )
 
-                                ExtensionVal fun ->
-                                    context
-                                        |> evalExp exp1
-                                        |> Result.andThen
-                                            (\v1 ->
-                                                fun v1
-                                            )
+                            ExtensionVal fun ->
+                                context
+                                    |> evalExp exp1
+                                    |> Result.andThen
+                                        (\v1 ->
+                                            fun v1
+                                        )
 
-                                _ ->
-                                    "Can't apply a value to "
-                                        ++ Internal.Util.valueToString v2
-                                        |> Err
-                        )
+                            _ ->
+                                "Can't apply a value to "
+                                    ++ Internal.Util.valueToString v2
+                                    |> Err
+                    )
 
 
 
